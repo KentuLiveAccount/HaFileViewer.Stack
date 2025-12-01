3f7f9e849fea6728e378b5ec832d81abbf52d02f
Kentaro Urata <kentu@microsoft.com>
2025-11-30 22:33:05 -0800

    add error handling

--- app/web/Main.hs
+++ app/web/Main.hs
@@ -15,6 +15,8 @@ import Data.Monoid ((<>))
 import Data.Aeson (object, (.=))
 import Network.HTTP.Types.Status (status400)
 import qualified Data.Text as T
+import Data.List (isInfixOf)
+import Control.Monad (when)
 import Data.Maybe (fromMaybe)
 import Text.Read (readMaybe)
 
@@ -24,6 +26,7 @@ main = do
   scotty 3000 $ do
     middleware logStdoutDev
     get "/open" $ do
+      setHeader "Access-Control-Allow-Origin" "*"
       ps <- params
       let mpath = lookup ("path" :: T.Text) ps
       case mpath of
@@ -32,10 +35,14 @@ main = do
           json (object ["error" .= ("missing path" :: String)])
         Just p -> do
           let path = T.unpack p
+          when (".." `isInfixOf` path) $ do
+            status status400
+            json (object ["error" .= ("invalid path" :: String)])
           fh <- liftIO $ openFileHandle path
           json $ object ["size" .= (fileSize fh)]
 
     get "/range" $ do
+      setHeader "Access-Control-Allow-Origin" "*"
       ps <- params
       case lookup ("path" :: T.Text) ps of
         Nothing -> do
@@ -43,7 +50,10 @@ main = do
           json (object ["error" .= ("missing path" :: String)])
         Just p -> do
           let path = T.unpack p
-              off = fromMaybe 0 (lookup "off" ps >>= (readMaybe . T.unpack)) :: Integer
+          when (".." `isInfixOf` path) $ do
+            status status400
+            json (object ["error" .= ("invalid path" :: String)])
+          let off = fromMaybe 0 (lookup "off" ps >>= (readMaybe . T.unpack)) :: Integer
               len = fromMaybe 4096 (lookup "len" ps >>= (readMaybe . T.unpack)) :: Int
           -- For /range we use the Backend file handle API to read bytes
           fh <- liftIO $ openFileHandle path
@@ -53,12 +63,16 @@ main = do
 
     -- lines endpoint: start can be negative
     get "/lines" $ do
+      setHeader "Access-Control-Allow-Origin" "*"
       ps <- params
       case lookup ("path" :: T.Text) ps of
         Nothing -> do status status400; json (object ["error" .= ("missing path" :: String)])
         Just p -> do
           let path = T.unpack p
-              start = fromMaybe 0 (lookup "start" ps >>= (readMaybe . T.unpack)) :: Integer
+          when (".." `isInfixOf` path) $ do
+            status status400
+            json (object ["error" .= ("invalid path" :: String)])
+          let start = fromMaybe 0 (lookup "start" ps >>= (readMaybe . T.unpack)) :: Integer
               cnt = fromMaybe 100 (lookup "count" ps >>= (readMaybe . T.unpack)) :: Int
           mLm <- liftIO $ readIORef cacheRef
           lm <- case mLm of
